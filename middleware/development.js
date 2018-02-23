8d95330993e4d7c5ec0d5f0255411a8e5b930c67
Javier Cohen <javy1103@gmail.com>
2018-02-23 13:29:09 -0600

    saving work

---
+++ middleware/development.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const config = require('../config')
+if (!process.env.NODE_ENV) {
+  process.env.NODE_ENV = JSON.parse(config.dev.env.NODE_ENV)
+}
+
+const webpack = require('webpack')
+const webpackConfig = process.env.NODE_ENV === 'testing'
+  ? require('../build/webpack.prod.conf')
+  : require('../build/webpack.dev.conf')
+
+const compiler = webpack(webpackConfig)
+
+const devMiddleware = require('webpack-dev-middleware')(compiler, {
+  publicPath: webpackConfig.output.publicPath,
+  quiet: true
+})
+
+const hotMiddleware = require('webpack-hot-middleware')(compiler, {
+  log: false
+})
+// force page reload when html-webpack-plugin template changes
+compiler.plugin('compilation', function (compilation) {
+  compilation.plugin('html-webpack-plugin-after-emit', function (data, cb) {
+    hotMiddleware.publish({ action: 'reload' })
+    cb()
+  })
+})
+
+module.exports = [
+  hotMiddleware,
+  devMiddleware
+]
---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <header>
+      <span>Vue.js PWA</span>
+    </header>
+    <main>
+      <img src="./assets/logo.png" alt="Vue.js PWA">
+      <router-view></router-view>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style lang="scss">
+@import '../node_modules/bootstrap-sass/assets/stylesheets/_bootstrap.scss';
+@import './assets/sass/main.scss';
+</style>
